de4710ec3f5ee57bfaa00ead15539cd0d699eb45
lowzonenose <jpbazonnais@gmail.com>
2019-09-06 14:55:56 +0200

    maj dep projet simple-map-leaflet-meteor

--- simple-map-leaflet-meteor/client/main.js
+++ simple-map-leaflet-meteor/client/main.js
@@ -1,8 +1,8 @@
 // leaflet
-import L from "leaflet/dist/leaflet-src";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 // extensions
-import Gp from "geoportal-extensions-leaflet/dist/GpPluginLeaflet-src";
+import { Services, LExtended } from "geoportal-extensions-leaflet";
 import "geoportal-extensions-leaflet/dist/GpPluginLeaflet-src.css";
 
 import './main.html';
@@ -10,7 +10,7 @@ import './main.html';
 function createMap () {
 
     // Création de la map
-    var layer = L.geoportalLayer.WMS({
+    var layer = LExtended.geoportalLayer.WMS({
       layer : "ORTHOIMAGERY.ORTHOPHOTOS"
     });
 
@@ -21,25 +21,25 @@ function createMap () {
 
     layer.addTo(map);
 
-    // Ajout des extensions 
-    var iso = L.geoportalControl.Isocurve();
+    // Ajout des extensions
+    var iso = LExtended.geoportalControl.Isocurve();
     map.addControl(iso);
-    var layerSwitcher = L.geoportalControl.LayerSwitcher();
+    var layerSwitcher = LExtended.geoportalControl.LayerSwitcher();
     map.addControl(layerSwitcher);
-    var mp = L.geoportalControl.MousePosition();
+    var mp = LExtended.geoportalControl.MousePosition();
     map.addControl(mp);
-    var route = L.geoportalControl.Route();
+    var route = LExtended.geoportalControl.Route();
     map.addControl(route);
-    var reverse = L.geoportalControl.ReverseGeocode();
+    var reverse = LExtended.geoportalControl.ReverseGeocode();
     map.addControl(reverse);
-    var search = L.geoportalControl.SearchEngine();
+    var search = LExtended.geoportalControl.SearchEngine();
     map.addControl(search);
-    var measureProfil = L.geoportalControl.ElevationPath();
+    var measureProfil = LExtended.geoportalControl.ElevationPath();
     map.addControl(measureProfil);
 }
 
 // Pour la configuration des services
-Gp.Services.getConfig({
+Services.getConfig({
   apiKey : "jhyvi0fgmnuxvfv0zjzorvdn",
   onSuccess : createMap
 });
