b1fed598a919b2e39c1ea13047b9603645550dbe
lowzonenose <jpbazonnais@gmail.com>
2019-09-06 15:01:34 +0200

    maj deps projet simple-map-leaflet-meteor-react

--- simple-map-leaflet-meteor-react/imports/ui/App.js
+++ simple-map-leaflet-meteor-react/imports/ui/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
  // leaflet
-import L from "leaflet/dist/leaflet-src";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 // extensions
-import Gp from "geoportal-extensions-leaflet/dist/GpPluginLeaflet-src";
+import { Services, LExtended } from "geoportal-extensions-leaflet";
 import "geoportal-extensions-leaflet/dist/GpPluginLeaflet-src.css";
 
 // App component - represents the whole app
@@ -13,36 +13,36 @@ export default class App extends Component {
     function createMap () {
 
         // Création de la map
-        var layer = L.geoportalLayer.WMS({
+        var layer = LExtended.geoportalLayer.WMS({
           layer : "ORTHOIMAGERY.ORTHOPHOTOS"
         });
-    
+
         var map  = L.map('map', {
           zoom : 19,
           center : L.latLng(48.8449, 2.4245)
         });
-    
+
         layer.addTo(map);
-    
-        // Ajout des extensions 
-        var iso = L.geoportalControl.Isocurve();
+
+        // Ajout des extensions
+        var iso = LExtended.geoportalControl.Isocurve();
         map.addControl(iso);
-        var layerSwitcher = L.geoportalControl.LayerSwitcher();
+        var layerSwitcher = LExtended.geoportalControl.LayerSwitcher();
         map.addControl(layerSwitcher);
-        var mp = L.geoportalControl.MousePosition();
+        var mp = LExtended.geoportalControl.MousePosition();
         map.addControl(mp);
-        var route = L.geoportalControl.Route();
+        var route = LExtended.geoportalControl.Route();
         map.addControl(route);
-        var reverse = L.geoportalControl.ReverseGeocode();
+        var reverse = LExtended.geoportalControl.ReverseGeocode();
         map.addControl(reverse);
-        var search = L.geoportalControl.SearchEngine();
+        var search = LExtended.geoportalControl.SearchEngine();
         map.addControl(search);
-        var measureProfil = L.geoportalControl.ElevationPath();
+        var measureProfil = LExtended.geoportalControl.ElevationPath();
         map.addControl(measureProfil);
     }
-    
+
     // Pour la configuration des services
-    Gp.Services.getConfig({
+    Services.getConfig({
         apiKey : "jhyvi0fgmnuxvfv0zjzorvdn",
         onSuccess : createMap
     });
